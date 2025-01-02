a3a774be15f9111199dfc26b502d2a59994220ca
Philip Trinh <philipgtrinh@gmail.com>
2025-01-02 00:36:11 -0600

    small change

--- constants.py
+++ constants.py
@@ -1,1 +1,11 @@
+import os
+from chromadb.config import Settings
+
+#Define chroma settings
+chroma_settings = Settings(
+    chroma_db_impl = 'duckdb+parquet',
+    persist_directory = "db",
+    anonymized_telemetry = False
+)
+
 
